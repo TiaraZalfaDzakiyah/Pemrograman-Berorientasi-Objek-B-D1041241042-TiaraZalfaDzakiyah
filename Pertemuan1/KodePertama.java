64aa2da4f0bd94b055697449140f561cd8380101
TiaraZalfaDzakiyah <d1041241042@student.untan.ac.id>
2026-02-05 14:33:07 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,11 @@
+/*Nama: Tiara Zalfa Dzakiyah
+NIM: D1041241042
+Hari/Tgl: Kamis/5 Februari 2026 */
+
+package Pertemuan1;
+public class KodePertama {
+    public static void main(String[] args) {
+        System.out.println("Halo, Pemrograman Berorientasi Objek");
+        System.out.println("Ini adalah program Java pertama saya");
+    }
+}
---
+++ Pertemuan1/KodePertama_Latihan1.java
@@ -0,0 +1,13 @@
+/*Nama: Tiara Zalfa Dzakiyah
+NIM: D1041241042
+Hari/Tgl: Kamis/5 Februari 2026 */
+
+package Pertemuan1;
+public class KodePertama_Latihan1 {
+    public static void main(String[] args) {
+        System.out.println("Nama: Tiara Zalfa Dzakiyah");
+        System.out.println("NIM: D1041241042");
+        System.out.println("Semester: 4");
+        System.err.println("Prodi: Informatika");
+    }
+}
