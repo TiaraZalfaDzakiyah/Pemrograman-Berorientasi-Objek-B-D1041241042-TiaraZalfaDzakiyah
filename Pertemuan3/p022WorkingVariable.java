6c023417ad6b4aa0d2678712727e3877673f29e1
TiaraZalfaDzakiyah <d1041241042@student.untan.ac.id>
2026-02-12 14:12:32 +0700

    Pertemuan3

---
+++ Pertemuan3/p022WorkingVariable.java
@@ -0,0 +1,21 @@
+/*Nama: Tiara Zalfa Dzakiyah
+NIM: D1041241042 */
+
+
+package Pertemuan3;
+
+public class p022WorkingVariable {
+    public static void main(String[] args) {
+        //Deklarasi dan inisialisasi variabel
+        int angka = 10;
+
+        //Menampilkan nilai awal variabel
+        System.out.println("Nilai awal angka: " + angka);
+
+        //Mengubah nilai variabel
+        angka = 25;
+
+        //Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
