a60c0e4fd91ac913b7724dc72eb78a486face337
TiaraZalfaDzakiyah <d1041241042@student.untan.ac.id>
2026-02-18 19:07:25 +0700

    Latihan 1 dan 2 Pertemuan4

---
+++ Pertemuan4/LatihanKalkulator.java
@@ -0,0 +1,18 @@
+package Pertemuan4;
+
+public class LatihanKalkulator {
+    public static void main(String[] args) {
+        System.out.println("=== KALKULATOR ===");
+        int angka1 = 25;
+        int angka2 = 4;
+        System.out.println("Angka 1 : " + angka1);
+        System.out.println("Angka 2 : " + angka2);
+        System.out.println("---");
+        System.out.println("Penjumlahan : " + (angka1+angka2));
+        System.out.println("Pengurangan : " + (angka1-angka2));
+        System.out.println("Perkalian : " + (angka1*angka2));
+        System.out.println("Pembagian : " + (angka1/angka2));
+        System.out.println("Sisa Bagi : " + (angka1%angka2));
+        System.out.println("Pembagian Desimal : " + ((double)angka1/angka2));
+    }
+}
---
+++ Pertemuan4/LatihanProduk.java
@@ -0,0 +1,20 @@
+package Pertemuan4;
+
+public class LatihanProduk {
+    public static void main(String[] args) {
+        System.out.println("=== INFORMASI PRODUK ===");
+        String namaProduk = "Laptop Asus";
+        int harga = 7000000; 
+        int jumlahStok = 15; 
+        boolean isState = true; 
+        System.out.println("Nama Produk : " + namaProduk);
+        System.out.println("Harga Satuan : Rp " + harga);
+        System.out.println("Jumlah Stok : " + jumlahStok);
+        System.out.println("Status Tersedia : " + isState);
+        System.out.println("---");
+        long totalNilai = (long)harga*jumlahStok;
+        double PAJAK = 0.11;
+        System.out.println("Total Nilai Stok : Rp " + totalNilai);
+        System.out.println("Pajak (11%) : Rp " + ((long)(totalNilai*PAJAK)));
+    }
+}
